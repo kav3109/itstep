7b2fcd1670c61cf2e3aafc759953ef27d61ecc2f
Artest <kav3109@gmail.com>
2019-12-26 23:33:47 +0200

    241219(30%)

--- practice/test.js
+++ practice/test.js
@@ -1,6 +1,33 @@
-for (let k = 2; k <= 9; k++){
-    for (let m = 1; m <=10; m++)
-    {
-        console.log(k + "*" + m + "=" +(k*m));
-    }
-}
+// 1.
+// let n = 3;
+// for (let i = 0; i < n; i++) {
+//     console.log('#')
+// }
+// let r = 3;
+// while(r>0) {
+//     console.log('>');
+//     r--;
+// }
+// 2.
+// let n = 6;
+// do {
+//     console.log(n);
+//     n--;
+// }
+// while(n>=0);
+// 3.
+// let x = 6;
+// let n = 0;
+// let result = 1;
+// while(n>0) {
+//     result = result*x;
+//     n--;
+// }
+// console.log(result);
+// 4.
+// let n = 4;
+// let result = 1;
+// for(let i = 1; i <= n; i++) {
+//     result = result*i;
+// }
+// console.log(result);
