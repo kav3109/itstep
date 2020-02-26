1413e4fedaf37c0ecfbb605e231a86abf366bdee
Artest <kav3109@gmail.com>
2020-02-26 20:28:22 +0200

    20022020(100%)

--- homework_20.02.2020/script.js
+++ homework_20.02.2020/script.js
@@ -1,4 +1,4 @@
-// 1. Circle
+// 1. Circle ******************************************************************
 
 class Circle {
     constructor(radius) {
@@ -27,24 +27,75 @@ class Circle {
 }
 const circle = new Circle(4);
 circle.radius = 5;
-console.log(`radius = ${circle.radius}; 
-diameter = ${circle.getDiameter()}; 
-area = ${circle.getArea()}; 
+console.log(`radius = ${circle.radius};
+diameter = ${circle.getDiameter()};
+area = ${circle.getArea()};
 length = ${circle.getPerimeter()};`);
 
-//  2.
-//   Задание 2
-// Определить класс Children, который содержит
-// такие поля: имя ребенка, фамилию и возраст ,
-//     публичные – методы ввода данных и
-// отображения их на экран. Объявить два объекта
-// класса, внести данные и показать их.
-// Задание 3
-// Реализовать класс, который описывает css класс.
-//     Класс CssClass должен содержать внутри себя:
-//     ■ название css класса;
-// ■ массив стилей;
-// ■ метод для установки стиля;
-// ■ метод для удаления стиля;
-// ■ метод getCss() , который возвращает css код в виде стро-
-// ки.
+//  2. Children ***************************************************************
+
+class Children {
+    constructor(name, surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(val) {
+        if(typeof val !== 'string') console.error('Wrong type of passed value!');
+        this._name = val;
+    }
+    get surname() {
+        return this._surname;
+    }
+    set surname(val) {
+        if(typeof val !== 'string') console.error('Wrong type of passed value!');
+        this._surname = val;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(val) {
+        if(typeof val !== 'number') console.error('Wrong type of passed value!');
+        this._age = val;
+    }
+}
+let girl = new Children('Olga', 'Petrova', 8);
+let boy = new Children('Oleg', 'Petrov', 9);
+console.log(`Girl: name - ${girl.name}, surname - ${girl.surname}, age - ${girl.age}`);
+console.log(`Boy: name - ${boy.name}, surname - ${boy.surname}, age - ${boy.age}`);
+
+// 3. Css *********************************************************************
+
+class CssClass {
+    constructor(name, styles) {
+        this.name = name;
+        this.styles = styles;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(val) {
+        if(typeof val !== 'string') console.error('Wrong type of passed value!');
+        this._name = val;
+    }
+    get styles() {
+        return this._styles;
+    }
+    set styles(val) {
+        if(typeof val !== 'object') console.error('Wrong type of passed value!');
+        this._styles = val;
+    }
+    deleteStyle() {
+        this._styles = [];
+    }
+    getCss() {
+        return `.${this.name} {${this.styles.join('')}}`
+    }
+}
+let myStyle = new CssClass('header', ['font-size:12px;', 'color:blue;']);
+console.log(myStyle.getCss());
+myStyle.deleteStyle();
+console.log(myStyle.getCss());
