e4db868ba83c56665e50e3a94b43c94d7fd9fb7d
Artest <kav3109@gmail.com>
2020-04-19 21:49:52 +0300

    hw07042020(10%)

--- homework_07.04.2020/js/scripts.js
+++ homework_07.04.2020/js/scripts.js
@@ -6,16 +6,12 @@ let btnSearch = document.querySelector('.btnSearch');
 let wrapper = document.getElementById('wrapper');
 let loader = document.querySelector('.loader');
 
-const urlParams = new URLSearchParams(window.location.search);
-const ident = urlParams.get('id');
-
-// if (ident) {
-    let xhr = new XMLHttpRequest();
-xhr.open('GET','http://www.omdbapi.com/?t=runFurious&apikey=9fb989c2');
-    // xhr.open('GET',`https://jsonplaceholder.typicode.com/users/${ident}`);
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET','http://www.omdbapi.com/?s=hello&page=2&apikey=9fb989c2');
     xhr.send();
-
-    xhr.onload = () => {
+//*********************************************1:08
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState !== 4) return;
         loader.style.display = 'none';
         if(xhr.status === 200) {
             let json = JSON.parse(xhr.response);
@@ -30,7 +26,7 @@ xhr.open('GET','http://www.omdbapi.com/?t=runFurious&apikey=9fb989c2');
     };
 
     function fillData(json) {
-        wrapper.innerText = json.name;
+        wrapper.innerText = json.totalResults;
     }
 
 // xhr.onprogress = (e) => {
