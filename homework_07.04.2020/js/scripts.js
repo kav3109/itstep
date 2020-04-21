b740044aca43c92549f6c062c9017fca668452b7
Artest <kav3109@gmail.com>
2020-04-22 00:42:32 +0300

    hw07042020(90%)

--- homework_07.04.2020/js/scripts.js
+++ homework_07.04.2020/js/scripts.js
@@ -5,11 +5,10 @@ let form = document.forms.searchMovie;
 form.addEventListener('submit', () => {
 
     event.preventDefault();
-    let title, type, result, loader, item, pagination;
+    let title, type, loader, item, pagination;
 
     title = document.getElementById('title');
     type = document.getElementById('type');
-    result = document.querySelector('.films');
     item = document.querySelector('.item');
     pagination = document.querySelector('.pagination');
 
@@ -21,7 +20,15 @@ form.addEventListener('submit', () => {
     loader.innerText = 'Loading...';
     document.body.appendChild(loader);
 
+    search();
+
+});
+
+const search = () => {
+
     getData(title.value, type.value, '1', (json) => {
+
+        let result = document.querySelector('.films');
 
         if(json.Response === 'False') {
             result.innerText = json.Error;
@@ -31,14 +38,33 @@ form.addEventListener('submit', () => {
             items.forEach((val, id, arr) => {
                 let poster = arr[id].Poster;
                 if(poster.match(/http/) === null) poster = 'image/no_poster_available.jpg';
-                setItem(poster, arr[id].Type, arr[id].Title, arr[id].Year);
+                setItem(poster, arr[id].Type, arr[id].Title, arr[id].Year, arr[id].imdbID);
             });
-            if(+json.totalResults > 10) setPagination(+json.totalResults);
+            let totalPages = Math.ceil(+json.totalResults/10);
+            if(totalPages > 1) {
+                setPagination(totalPages);
+                let pages = document.querySelectorAll('a');
+                pages.forEach((val,id,arr) => {
+                    arr[id].onclick =() => {
+                        let active = document.querySelector('.active');
+                        if(arr[id].className === 'back'){
+                            getData(title.value, type.value, moveBack(), (json)=>{});
+                        }
+                        if(arr[id].className === ''){
+                            getData(title.value, type.value, arr[id].innerText, (json)=>{});
+                            active.classList.remove('active');
+                            arr[id].className = 'active';
+                        }
+                        if(arr[id].className === 'forward'){
+                            getData(title.value, type.value, moveForward(totalPages), (json)=>{});
+                        }
+                    }
+                })
+            }
+            showInfo();
         }
     });
-});
-
-// get json************************************************
+};
 
 const getData = (search, type, page, success) => {
     const xhr = new XMLHttpRequest();
@@ -48,7 +74,7 @@ const getData = (search, type, page, success) => {
     xhr.onreadystatechange = () => {
         if(xhr.readyState !== 4) return;
         let loader = document.querySelector('.loader');
-        loader.remove();
+        if(loader !== null)loader.remove();
         if(xhr.status === 200) {
             let json = JSON.parse(xhr.response);
             success(json);
@@ -62,9 +88,41 @@ const getData = (search, type, page, success) => {
     };
 };
 
-//*********************************************************
+const getMovieInfo = (id, success) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET',`http://www.omdbapi.com/?i=${id}&apikey=9fb989c2`);
+    xhr.send();
 
-const setItem = (videoPoster, videoType, videoTitle, videoYear) => {
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState !== 4) return;
+        let loader = document.querySelector('.loader');
+        if(loader !== null)loader.remove();
+        if(xhr.status === 200) {
+            let json = JSON.parse(xhr.response);
+            success(json);
+        } else {
+            console.log(xhr.status+' '+xhr.statusText);
+        }
+    };
+
+    xhr.onerror = () => {
+        if(json.Response === 'False') {
+            result.innerText = json.Error;
+        } else {
+            // Poster
+            // Title
+            // Released
+            // Genre
+            // Country
+            // Director
+            // Writer
+            // Actors
+            // Awards
+        }
+    };
+};
+
+const setItem = (videoPoster, videoType, videoTitle, videoYear, videoId) => {
 
     let wrapper, item, poster, type, title, year, detail;
 
@@ -79,6 +137,7 @@ const setItem = (videoPoster, videoType, videoTitle, videoYear) => {
     title = document.createElement("H4");
     year = document.createElement("P");
     detail = document.createElement("INPUT");
+    detail.setAttribute("class", videoId);
     detail.setAttribute("type", "button");
     detail.setAttribute("value", "Details");
 
@@ -102,9 +161,11 @@ const deleteItems = () => {
     });
 };
 
-const setPagination = (count) => {
+const setPagination = (count, active) => {
 
     let wrapper, pagination, a, forward, back, wrapWidth = 105;
+
+    if(active === undefined) active = 1;
 
     wrapper = document.body;
     pagination = document.createElement("DIV");
@@ -113,39 +174,75 @@ const setPagination = (count) => {
 
     back = document.createElement("A");
     back.setAttribute("class", "back");
-    back.setAttribute("href", "#");
     back.innerText = '<<';
 
     forward = document.createElement("A");
     forward.setAttribute("class", "forward");
-    forward.setAttribute("href", "#");
     forward.innerText = '>>';
 
     pagination.appendChild(back);
-    (+count > 100)? count = 10: count = Math.ceil((+count)/10);
+    count = (+count > 10)? 10: count;
     for(let i = 1; i <= count; i++) {
         a = document.createElement("A");
-        a.setAttribute("href", "#");
-        if(i === 1) a.setAttribute("class", "active");
+        if(i === +active) a.setAttribute("class", "active");
         a.innerText = i;
         pagination.appendChild(a);
-        wrapWidth+=43;
+        wrapWidth+=50;//pagination button size
     }
     pagination.appendChild(forward);
     pagination.style.width = `${wrapWidth}px`
+};
 
-// <div class="pagination">
-//         <a href="#">&laquo;</a>
-//         <a href="#">1</a>
-//         <a href="#" class="active">2</a>
-//         <a href="#">3</a>
-//         <a href="#">4</a>
-//         <a href="#">5</a>
-//         <a href="#">6</a>
-//         <a href="#">7</a>
-//         <a href="#">8</a>
-//         <a href="#">9</a>
-//         <a href="#">10</a>
-//         <a href="#">&raquo;</a>
-//     </div>
+const moveForward = (totalPages) => {
+    let btns = document.querySelectorAll('a');
+    let activePage = document.querySelector('.active');
+    let activeNum = +activePage.innerText;
+    if(activeNum === +totalPages && +totalPages === btns.length-2) return activeNum;
+    if(+btns[1].innerText === 1 && +activeNum !== 10) {
+        activePage.classList.remove('active');
+        btns[activeNum+1].className = 'active';
+        return (activeNum+1);
+    }
+    activePage.classList.remove('active');
+    for (let i = 1; i < btns.length-1; i++) {
+        btns[i].innerText = +btns[i].innerText+1;
+        if(i === btns.length-2) {
+            btns[10].className = 'active';
+            return btns[10].innerText;
+        }
+    }
+};
+
+const moveBack = (totalPages) => {
+    let btns = document.querySelectorAll('a');
+    let activePage = document.querySelector('.active');
+    let activeNum = +activePage.innerText;
+    if(+activeNum === 1) return activeNum;
+    if(+btns[1].innerText === 1 && +activeNum !== 1) {
+        activePage.classList.remove('active');
+        btns[activeNum-1].className = 'active';
+        return (activeNum-1);
+    }
+    activePage.classList.remove('active');
+    for (let i = 1; i < btns.length-1; i++) {
+        btns[i].innerText = +btns[i].innerText-1;
+        if(i === btns.length-2) {
+            btns[1].className = 'active';
+            return btns[1].innerText;
+        }
+    }
+};
+
+const showInfo = () => {
+
+    let allItems = document.querySelectorAll('input[value="Details"]');
+
+    allItems.forEach((val,id,arr) => {
+        arr[id].onclick = () => {
+            getMovieInfo(arr[id].className, (json) => {
+                console.log(json.Response);
+            })
+        }
+    })
+
 };
