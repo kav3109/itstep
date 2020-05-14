9d47ce12cffe00d57e370a7729ad1a9a18c3d47a
Artest <kav3109@gmail.com>
2020-05-14 14:16:22 +0300

    hw12052020(100%)

---
+++ practice/jquery/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+    //1.
+    $('#btn').click(() => {
+       const $p = $('#p1');
+       $p.css({"color":"red","font-size":"34px"});
+    });
+    //2.
+    const $link = $('#link');
+    $link.hover(
+        () => {
+            $link.css('color', 'orange');
+        },
+        () => {
+            $link.css('color', 'blue');
+        });
+    //3.
+    $('#btn2').click(() => {
+        const $p2 = $('#p2');
+        ($('p.aaa').length === 0)? $p2.addClass("aaa"): $p2.removeClass("aaa");
+    });
+    //4.
+    $('li').each((ind, el)=>{
+        console.log(`${ind+1} ${$(el).text()}`);
+    });
+
+});
