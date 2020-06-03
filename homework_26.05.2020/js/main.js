f42cb391401841d62d2cd6c9edf3877fd9cce3ec
Artest <kav3109@gmail.com>
2020-06-04 00:32:33 +0300

    hw26052020(10%)

--- homework_26.05.2020/js/main.js
+++ homework_26.05.2020/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(() => {
 
+    const imagesArr = [
+        'https://i.picsum.photos/id/1018/1920/1080.jpg',
+        'https://i.picsum.photos/id/1019/1920/1080.jpg',
+        'https://i.picsum.photos/id/1021/1920/1080.jpg',
+    ];
 
+    //expand or compress controller
+    $('.screen').click(function() {
+        $('.expand').toggleClass('hidden');
+        $('.compress').toggleClass('hidden');
+        $('.wrapper').toggleClass('full-wrap');
+        $('.presentation, img').toggleClass('full-presentation');
+    });
 
 });
