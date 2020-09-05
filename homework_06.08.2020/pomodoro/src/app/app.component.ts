514240d3735b89cc833519df5c560a3e0a669f49
Artest <kav3109@gmail.com>
2020-09-05 13:41:09 +0300

    hw_06_08_2020(100%)

--- homework_06.08.2020/pomodoro/src/app/app.component.ts
+++ homework_06.08.2020/pomodoro/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {compareNumbers} from "@angular/compiler-cli/src/diagnostics/typescript_version";
 import {Sections} from "./sections";
 
@@ -7,33 +7,27 @@ import {Sections} from "./sections";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pomodoro';
 
-  sec: number = 50; // for test 1sec = 100ms
+  sec: number = 50; // for test 1sec = 50ms TODO set 1000 for production
   started: boolean = false;
-  selectedWorkTime: number = 2;
+  selectedWorkTime: number = 1;
   selectedShortTimeout: number = 1;
-  selectedLongTimeout: number = 3;
-  mmStart: number = this.selectedWorkTime;
-  ssStart: number = 0;
-  timer: number; //set interval for section
-  pomoTimer: number; //set interval for pomodora
-  counter: number = 0; //
-  sections: Array<Sections> = [
-    {id: 0, started: false, time: this.selectedWorkTime, type: 'work'},
-    {id: 1, started: false, time: this.selectedShortTimeout, type: 'coffee'},
-    {id: 2, started: false, time: this.selectedWorkTime, type: 'work'},
-    {id: 3, started: false, time: this.selectedShortTimeout, type: 'coffee'},
-    {id: 4, started: false, time: this.selectedWorkTime, type: 'work'},
-    {id: 5, started: false, time: this.selectedShortTimeout, type: 'coffee'},
-    {id: 6, started: false, time: this.selectedWorkTime, type: 'work'},
-    {id: 7, started: false, time: this.selectedLongTimeout, type: 'break'}
-  ];
-
+  selectedLongTimeout: number = 1;
+  mmStart: number = 1;// helper for iteration timer
+  ssStart: number = 0; // helper for iteration timer
+  timer: number; //set interval for iteration
+  pomoTimer: number; //set global interval
+  counter: number = 0; //helper for global timer
+  theme: string = 'white';
+  themes: Array<string> = ['red', 'yellow', 'green', 'pink'];
+  intervals: Array<number> = [2, 3, 4, 5, 10, 15, 20, 25, 30, 60];
+  sections: Array<Sections>;
 
   startPomodoro(): void {
     this.started = true;
+    this.setTimers();
     this.pomoTimer = setInterval(() => {
       let iteration: any = this.sections[this.counter];
       if(this.counter === this.sections.length) return this.stopPomodoro();
@@ -65,7 +59,7 @@ export class AppComponent {
         this.ssStart = 60;
       }
       this.ssStart -= 1;
-    }, this.sec);//TODO set param for production 1000
+    }, this.sec);
   }
 
   stopPomodoro():void {
@@ -75,16 +69,7 @@ export class AppComponent {
     this.counter = 0;
     this.ssStart = 0;
     this.mmStart = this.selectedWorkTime;
-    this.sections = [
-      {id: 0, started: false, time: this.selectedWorkTime, type: 'work'},
-      {id: 1, started: false, time: this.selectedShortTimeout, type: 'coffee'},
-      {id: 2, started: false, time: this.selectedWorkTime, type: 'work'},
-      {id: 3, started: false, time: this.selectedShortTimeout, type: 'coffee'},
-      {id: 4, started: false, time: this.selectedWorkTime, type: 'work'},
-      {id: 5, started: false, time: this.selectedShortTimeout, type: 'coffee'},
-      {id: 6, started: false, time: this.selectedWorkTime, type: 'work'},
-      {id: 7, started: false, time: this.selectedLongTimeout, type: 'break'}
-    ];
+    this.setTimers(1);
   }
 
   playAudio(res: string): void{
@@ -94,5 +79,28 @@ export class AppComponent {
     if(res === 'break') audio.src = "../../assets/break.mp3";
     audio.load();
     audio.play();
+  }
+
+  setTimers(time?:number): void {
+    if(typeof time === "number")
+      this.mmStart = this.selectedLongTimeout=this.selectedShortTimeout=this.selectedWorkTime=time;
+    this.sections = [
+      {id: 0, started: false, time: this.selectedWorkTime, type: 'work'},
+      {id: 1, started: false, time: this.selectedShortTimeout, type: 'coffee'},
+      {id: 2, started: false, time: this.selectedWorkTime, type: 'work'},
+      {id: 3, started: false, time: this.selectedShortTimeout, type: 'coffee'},
+      {id: 4, started: false, time: this.selectedWorkTime, type: 'work'},
+      {id: 5, started: false, time: this.selectedShortTimeout, type: 'coffee'},
+      {id: 6, started: false, time: this.selectedWorkTime, type: 'work'},
+      {id: 7, started: false, time: this.selectedLongTimeout, type: 'break'}
+    ]
+  }
+
+  setColor(str1:string, str2:string): void {
+    this.themes[this.themes.indexOf(str2)] = str1; // delete pair value in combobox;
+  }
+
+  ngOnInit(): void {
+    this.setTimers();
   }
 }
